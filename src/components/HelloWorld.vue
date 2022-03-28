1247da5e2db8be032530cae063ed918f9f55dadf
zjhjszwx <526084500@qq.com>
2022-03-28 17:09:00 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 class="h1">{{ count }}</h1>
+</template>
+
+<style lang="scss" scoped>
+.h1 {
+  color: #42b983;
+}
+</style>
